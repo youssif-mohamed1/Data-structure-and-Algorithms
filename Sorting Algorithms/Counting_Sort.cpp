27dab23f865d70c2558d9f1f338b18eae233b43f
Youssef Mohamed Ahmed Ismail <108690456+youssif-mohamed1@users.noreply.github.com>
2023-10-27 15:40:23 +0200

    Add Sorting Algorithms/Counting_Sort.cpp

---
+++ Sorting Algorithms/Counting_Sort.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+const int N = 10, MAX_INT=1e6;
+
+// sorting array of numbers using counting sort
+int arr[N] = { 9,2,9,1,9,5,9,2,9,7 };
+
+// should be initialized by the maximum number of the array, but I initialized it by MAX_INT to be easier
+int prefix[MAX_INT];
+
+void counting_sort(int* arr,int l,int r) { // O(n)
+	for (int i = l; i < r; i++) {
+		prefix[arr[i]]++; // initializing the frequency of each element
+	}
+	for (int i = l+1; i < r; i++) {
+		prefix[i] += prefix[i - 1]; // prefix sum of the frequency array
+	}
+	// iterating for putting elements in their correct posistion
+	for (int i = r; i >= l; i--) {
+		if (i > l) {
+			while (prefix[i] > prefix[i - 1]) {
+				arr[--prefix[i]] = i;
+			}
+			continue;
+		}
+		while (prefix[i] > 0) {
+			arr[--prefix[i]] = i;
+		}
+	}
+}
+
+signed main() {
+	counting_sort(arr, 0, N);
+	for (int i = 0; i < N; i++) {
+		cout << arr[i] << " ";
+	}
+}
