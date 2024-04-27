f9d399fd38fcc3a8fa0e0cf061e554147d0cc2d8
Youssef Mohamed Ahmed Ismail <108690456+youssif-mohamed1@users.noreply.github.com>
2024-04-27 23:13:34 +0300

    Add Disjoint_Set.cpp

---
+++ Disjoint_Set.cpp
@@ -0,0 +1,81 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+class DSU {
+private:
+    vector<int> rank, parent, size;
+public:
+    DSU(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        } else if (rank[ulp_u] > rank[ulp_v]) {
+            parent[ulp_v] = ulp_u;
+        } else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_v]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        } else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+
+};
+
+void solve() {
+    DSU ds(7);
+    ds.unionByRank(1, 2);
+    ds.unionByRank(2, 3);
+    ds.unionByRank(4, 5);
+    ds.unionByRank(6, 7);
+    ds.unionByRank(5, 6);
+    if (ds.findUPar(3) == ds.findUPar(7)) {
+        cout << "Same\n";
+    } else {
+        cout << "Not Same\n";
+    }
+    ds.unionByRank(3, 7);
+    if (ds.findUPar(3) == ds.findUPar(7)) {
+        cout << "Same\n";
+    } else {
+        cout << "Not Same\n";
+    }
+}
+
+signed main() {
+    int t = 1;
+    //cin >> t;
+    for (int i = 1; i <= t; i++) {
+        solve();
+        cout<<endl;
+    }
+}
